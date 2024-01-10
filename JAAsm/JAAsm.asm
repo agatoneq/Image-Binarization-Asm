ea5aaafa851cabd544baf0c49d792d0472e346b2
agatoneq <agata.sobczyk444@wp.pl>
2024-01-10 03:33:27 +0100

    z błędem

--- JAAsm/JAAsm.asm
+++ JAAsm/JAAsm.asm
@@ -6,23 +6,31 @@
 ;r9 = rozmiar w bajtach
 ;int[] mm0 = { 11, 59, 30 };
 
-	    mov r10, 0		                                ; index ustwiwamy na 0
-        mov rsi, 0                                      ; licznik pikseli do 3    
+	    mov r10, rdx		                                ; index ustwiwamy na 0
+        mov rsi, 0  
+        mov rbp, 0                              ; licznik pikseli do 3    
         mov rax, 0 
         mov rbx, 0 
         mov cx, 3
         mov r11w, 0
+        mov rdi, r9
+        movq xmm0, rdx
+        mov rdx, 0
 
     mainLoop:
             cmp rsi, 3
             je loopEnd
-            mov bl, byte ptr [rcx + r10]
+            cmp rbp, rdi
+            jae loopEnd
+            mov bl, byte ptr [rcx + rbp]
+            inc rbp
             add ax, bx
             inc rsi
             jmp mainLoop
 
     loopEnd:
             mov rsi, 0
+            ;xor dx, dx
             div cx
             cmp al, 128
             jg changeToWhite
@@ -30,16 +38,15 @@
             je outerState
 
     outerState:
-            mov byte ptr [rdx + r10], al
+            mov byte ptr [r10 + rbp], al
             mov rax, 0
-            inc r10
             dec r9d
             jnz mainLoop
             ret
         
     changeToWhite:
             mov r11w, 0ffffh
-            cmovc ax, r11w
+            mov ax, r11w
             jmp outerState
 
 
